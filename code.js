a3982694b3df6a64f87652510850cfc9f69b56ee
Ryan Jay <ryan.jay@ontariotechu.net>
2020-07-19 17:26:08 -0400

    block move

--- code.js
+++ code.js
@@ -4,6 +4,7 @@ var spaces=[];
 var options = ["a","b","c","d","e","f"]
 var first;
 var second;
+var toDelete=[];
 
 function loadGame() {
   document.getElementById("start").disabled=true;
@@ -25,8 +26,10 @@ function loadGame() {
         var img = this.innerHTML;
         this.innerHTML = spaces[secondSquare].innerHTML;
         spaces[secondSquare].innerHTML=img;
+        moveCheck()
         first=null;
         second=null;
+
       }
 
     },false);
@@ -37,6 +40,68 @@ function loadGame() {
 
 
 }
+
+
+function moveCheck(){
+  for(i=0;i<8;i++){
+    for(j=0;j<6;j++){
+      firsts = spaces[i*8+j].children[0].getAttribute("src")
+      seconds = spaces[i*8+j+1].children[0].getAttribute("src")
+      thirds = spaces[i*8+j+2].children[0].getAttribute("src")
+      //console.log(spaces[i*8+j].children[0].getAttribute("src"))
+      //console.log(firsts)
+      if(firsts==seconds && firsts == thirds){
+        console.log("pair")
+        toDelete.push(spaces[i*8+j].value)
+        toDelete.push(spaces[i*8+j+1].value)
+        toDelete.push(spaces[i*8+j+2].value)
+      }
+    }
+  }
+  for(i=0;i<6;i++){
+    for(j=0;j<8;j++){
+      firsts = spaces[i*8+j].children[0].getAttribute("src")
+      seconds = spaces[(i+1)*8+j].children[0].getAttribute("src")
+      thirds = spaces[(i+2)*8+j].children[0].getAttribute("src")
+      if(firsts==seconds && firsts == thirds){
+        console.log("pair")
+        toDelete.push(spaces[i*8+j].value)
+        toDelete.push(spaces[(i+1)*8+j].value)
+        toDelete.push(spaces[(i+2)*8+j].value)
+      }
+    }
+  }
+  console.log(toDelete)
+  let tmp = [... new Set(toDelete)]
+  for(i=0;i<tmp.length;i++){
+    console.log(tmp[i])
+    console.log(spaces[Number(tmp[i])])
+    spaces[Number(tmp[i])].children[0].setAttribute("src","blank.jpg");
+  }
+
+  for(i=7;i>0;i--){
+    for(j=7;j>=0;j--){
+      if(spaces[i*8+j].children[0].getAttribute("src")=="blank.jpg"){
+        count=1;
+        while(i-count>=0&&spaces[(i-count)*8+j].children[0].getAttribute("src")=="blank.jpg"){
+          count++;
+        }
+        if(i-count>=0){
+          tmp = spaces[(i-count)*8+j].children[0].getAttribute("src")
+          tmp2 = spaces[i*8+j].children[0].getAttribute("src")
+          spaces[(i-count)*8+j].children[0].setAttribute("src",tmp2)
+          spaces[i*8+j].children[0].setAttribute("src",tmp)
+        }
+
+        console.log(i*8+j)
+      }
+    }
+  }
+  toDelete=[];
+}
+
+
+
 function chooseSecond(firstLoc,secondLoc){
   var difX = first[1]-second[1]
   var difY = first[2]-second[2]
@@ -55,4 +120,7 @@ function chooseSecond(firstLoc,secondLoc){
       return Number(first[0])+8;
     }
   }
+}
+function sleep (time) {
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
